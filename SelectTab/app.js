a28e912e0bef09b3c323e0349d9dc53760a35231
lovemgr6390 <36497170+lovemgr6390@users.noreply.github.com>
2024-05-21 18:46:52 +0530

    Final
    Final Struggle

---
+++ SelectTab/app.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll(".btn");
+const cont = document.querySelector(".right");
+const allsub = document.querySelectorAll(".content");
+
+cont.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  console.log(id);
+  if (id) {
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+
+    allsub.forEach((context) => {
+      context.classList.remove("active");
+    });
+    const el = document.getElementById(id);
+    el.classList.add("active");
+  }
+});
