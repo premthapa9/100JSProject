e71e445575b6feae668ecd5f8be536e4ba4956c1
lovemgr6390 <36497170+lovemgr6390@users.noreply.github.com>
2024-05-25 12:29:59 +0530

    Enhance
    Enhance

--- Feedback_UI/app.js
+++ Feedback_UI/app.js
@@ -3,16 +3,23 @@ let feedback = "";
 const btn = document.querySelector(".btn");
 
 const cont = document.querySelector(".container");
-
-labels.forEach((element) => {
-  const item = element.querySelector("input");
-  item.addEventListener("change", (e) => {
-    feedback = e.target.getAttribute("id");
-    console.log(feedback);
-  });
-});
+// const checked = document.querySelector("input:checked");
+// console.log(checked);
+// labels.forEach((element) => {
+//   const item = element.querySelector("input");
+//   item.addEventListener("change", (e) => {
+//     feedback = e.target.getAttribute("id");
+//     console.log(feedback);
+//   });
+// });
 
 btn.addEventListener("click", () => {
+  feedback = document.querySelector(
+    "input[type='radio'][name=feedback]:checked"
+  ).value;
+  console.log(
+    document.querySelector("input[type='radio'][name=feedback]:checked").value
+  );
   if (feedback !== "") {
     cont.innerHTML = `
     <strong>Thank you!</strong>
